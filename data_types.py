8349c2e822b5c04d325a4926ea8935bafbe00823
sidharthmallela <sidharth.mallela@gmail.com>
2021-01-10 18:36:55 -0800

    Data Types

---
+++ data_types.py
@@ -0,0 +1,9 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+input_int = int(input())
+input_double = float(input())
+input_string=str(input())
+print(i + input_int)
+print(d + input_double)
+print(s + input_string)
